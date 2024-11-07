319c41aae3746c171ffcf9cd806be6f0009a6fd3
olivermichel95 <olivermichel95@gmail.com>
2024-11-07 16:55:39 +0200

    for deployment on github pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+const { defineConfig } = require("vite");
+const react = require("@vitejs/plugin-react");
 
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   root: "src", // Keep source code in 'src' directory
   build: {
     outDir: "../dist", // Build output to 'dist' directory at the root level
     emptyOutDir: true, // Clean the 'dist' directory before building
+    base: "/adopt-me/", // Set the base path for GitHub Pages
   },
 });
