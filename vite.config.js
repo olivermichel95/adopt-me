650c22fd2ac32154585192a6d6a2abfe9fe21742
olivermichel95 <olivermichel95@gmail.com>
2024-11-07 15:06:51 +0200

    for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: "src",
+  root: "src", // Keep source code in 'src' directory
+  build: {
+    outDir: "../dist", // Build output to 'dist' directory at the root level
+    emptyOutDir: true, // Clean the 'dist' directory before building
+  },
 });
